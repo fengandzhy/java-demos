993712f8d2c9e5ec0ef38cb2d0246169a67eb520
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-06-25 06:38:38 +0800

    add a project about new features in java8 to the repository

---
+++ Java8/src/main/java/com/citi/java8/stream/filter/demo01/MultipFilterTest.java
@@ -0,0 +1,13 @@
+package com.citi.java8.stream.filter.demo01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultipFilterTest {
+
+	public static void main(String[] args) {
+		List<Person> pList = new ArrayList<>();
+
+	}
+
+}
