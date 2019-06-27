9ddf7fd19df7d0ecbee27a917676732d236a8f6b
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-06-27 11:56:30 +0800

    add an demo of bigdecimal substract

---
+++ JavaTest/src/main/java/com/citi/javatest/bigdecimal/demo01/BigDecimalDemo02.java
@@ -0,0 +1,14 @@
+package com.citi.javatest.bigdecimal.demo01;
+
+import java.math.BigDecimal;
+
+public class BigDecimalDemo02 {
+
+	public static void main(String[] args) {
+		BigDecimal a1 = new BigDecimal(2.623);
+		BigDecimal a2 = new BigDecimal(2);
+		System.out.println(a1.subtract(a2));
+		System.out.println(a1.subtract(a2).setScale(2,BigDecimal.ROUND_HALF_UP));
+	}
+
+}
