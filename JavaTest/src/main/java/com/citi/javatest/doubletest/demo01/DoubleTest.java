d9bf7187b6abfee379f414fe69f4875706047756
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-07-02 07:22:08 +0800

    integer transfer to double demo

---
+++ JavaTest/src/main/java/com/citi/javatest/doubletest/demo01/DoubleTest.java
@@ -0,0 +1,13 @@
+package com.citi.javatest.doubletest.demo01;
+
+public class DoubleTest {
+
+	/**
+	 * 把1000改成1000.00就会完成强制类型转换
+	 * */
+	public static void main(String[] args) {
+		Integer weight = 2132;
+		System.out.println(weight/1000.00);
+	}
+
+}
