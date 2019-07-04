31e01ad33f64213869762cc009be448dbf683264
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-07-05 06:36:10 +0800

    list demo

--- JavaTest/src/main/java/com/citi/javatest/doubletest/DoubleTest.java
+++ JavaTest/src/main/java/com/citi/javatest/doubletest/DoubleTest.java
@@ -6,8 +6,18 @@ public class DoubleTest {
 	 * 把1000改成1000.00就会完成强制类型转换
 	 * */
 	public static void main(String[] args) {
-		Integer weight = 2132;
-		System.out.println(weight/1000.00);
+//		Integer weight = 2132;
+//		System.out.println(weight/1000.00);
+		test01();
+	}
+	
+	public static void test01() {
+		int i = 16;
+		if(i%3!=0) {
+			
+		}
+		System.out.println(i/3);
+		System.out.println(i%3);
 	}
 
 }
