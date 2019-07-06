5f4ce829411b312d4a026307f460e753e377ecb8
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-07-06 20:01:28 +0800

    a comparator demo

---
+++ Java8/src/main/java/com/citi/java8/comparator/demo01/ComparatorTest.java
@@ -0,0 +1,10 @@
+package com.citi.java8.comparator.demo01;
+
+public class ComparatorTest {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Java8/src/main/java/com/citi/java8/comparator/demo01/Developer.java
@@ -0,0 +1,6 @@
+package com.citi.java8.comparator.demo01;
+
+public class Developer {
+	private double salary;
+	private int age;
+}	
