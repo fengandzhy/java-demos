38e211f5a8f5369c404be9ac9427dc9c7723d92b
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-07-10 07:54:43 +0800

    add two demos of type cast

--- CoreJava/src/main/java/com/citi/corejava/ch03/BigIntegerTest.java
+++ CoreJava/src/main/java/com/citi/corejava/ch03/BigIntegerTest.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class BigIntegerTest {
 
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.print("How many numbers do you need to draw? ");
---
+++ CoreJava/src/main/java/com/citi/corejava/ch03/test/TypeCastTest.java
@@ -0,0 +1,9 @@
+package com.citi.corejava.ch03.test;
+
+public class TypeCastTest {
+	
+	public static void main(String[] args) {
+		int i = 300;
+		System.out.println((byte)i);
+	}
+}
