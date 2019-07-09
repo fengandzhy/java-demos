00ba493888b10d5a48af082f39c4e981eb29ba7d
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-07-09 10:49:28 +0800

    parallelStream demo

---
+++ Java8/src/main/java/com/citi/java8/parallelStream/demo01/parallelStreamTest.java
@@ -0,0 +1,13 @@
+package com.citi.java8.parallelStream.demo01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class parallelStreamTest {
+	
+	public static void main(String args[]) {
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		numbers.parallelStream()
+		       .forEach(System.out::println); 
+	}
+}
--- Java8/src/main/java/com/citi/java8/stream/map/demo01/MapTest.java
+++ Java8/src/main/java/com/citi/java8/stream/map/demo01/MapTest.java
@@ -4,5 +4,4 @@ public class MapTest {
 
 	public static void main(String[] args) {
 	}
-
 }
