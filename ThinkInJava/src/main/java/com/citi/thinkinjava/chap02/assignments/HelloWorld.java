ee62d38e0bdd7550749040108ad72bd2ed778680
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-07-19 10:44:40 +0800

    assignment for think in java chapter02

---
+++ ThinkInJava/src/main/java/com/citi/thinkinjava/chap02/assignments/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.citi.thinkinjava.chap02.assignments;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ ThinkInJava/src/main/java/com/citi/thinkinjava/chap02/demos/HelloDate.java
@@ -0,0 +1,12 @@
+package com.citi.thinkinjava.chap02.demos;
+
+import java.util.Date;
+
+public class HelloDate {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,it's :");
+		System.out.println(new Date());
+	}
+
+}
