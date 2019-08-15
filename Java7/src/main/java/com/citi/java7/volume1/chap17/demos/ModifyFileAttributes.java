670b1918904e1f00b3caf98f21bade9fe7ac8969
fengandzhy <fengandzhy@gmail.com>
2019-08-15 22:23:12 +1200

    update file attributes

---
+++ Java7/src/main/java/com/citi/java7/volume1/chap17/demos/ModifyFileAttributes.java
@@ -0,0 +1,12 @@
+package com.citi.java7.volume1.chap17.demos;
+
+import javax.swing.JFrame;
+
+public class ModifyFileAttributes extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
