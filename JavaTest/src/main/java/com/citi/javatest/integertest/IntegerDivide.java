be8bd1f5dbffa0c3f9fe085ccb4871802ad91099
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-08-27 06:24:53 +0800

    a demo for do while cycyle

---
+++ JavaTest/src/main/java/com/citi/javatest/integertest/IntegerDivide.java
@@ -0,0 +1,11 @@
+package com.citi.javatest.integertest;
+
+public class IntegerDivide {
+
+	public static void main(String[] args) {
+		Integer a = 5;
+		Integer b = 2;
+		System.out.println(a/b);
+	}
+
+}
