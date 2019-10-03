bf1cb89c833c6aa7bba96e6689a59d1cd867f257
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2019-10-03 17:16:34 +1300

    a demo of getting specific day

---
+++ JavaDemos/Java8-demos/src/main/java/org/zhouhy/java8/localdate/getSpecificDate.java
@@ -0,0 +1,18 @@
+package org.zhouhy.java8.localdate;
+
+import java.time.LocalDate;
+
+public class getSpecificDate {
+
+	public static void main(String[] args) {
+		LocalDate dateOfBirth = LocalDate.of(2010, 01, 14);
+		System.out.println("Your Date of birth is : " + dateOfBirth); 
+		
+		int year = dateOfBirth.getYear(); 
+		int month = dateOfBirth.getMonthValue(); 
+		int day = dateOfBirth.getDayOfMonth();
+		
+		System.out.printf("Year : %d Month : %d day : %d \t %n", year, month, day); 
+	}
+
+}
