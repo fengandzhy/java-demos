c8105c8fdb2571de9dbb20edb3451f902f07c259
fengandzhy <fengandzhy@gmail.com>
2020-01-13 23:28:16 +1300

    java7 bigdecimal demo

---
+++ Java7/Java7-basic/src/main/java/org/zhouhy/bigdecimal/DoubleTest.java
@@ -0,0 +1,13 @@
+package org.zhouhy.bigdecimal;
+
+/**
+ * 这里不能加@Test因为不是测试的地方
+ * */
+public class DoubleTest {
+	
+	public static void main(String args[]){
+		
+		System.out.println(0.01+0.05);
+		
+	}
+}
