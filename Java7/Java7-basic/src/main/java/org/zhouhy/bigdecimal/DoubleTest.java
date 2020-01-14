84fd9cf9df1e2aa8de45ad5c75da9dc7b20ea553
fengandzhy <fengandzhy@gmail.com>
2020-01-14 22:46:23 +1300

    double demo

--- Java7/Java7-basic/src/main/java/org/zhouhy/bigdecimal/DoubleTest.java
+++ Java7/Java7-basic/src/main/java/org/zhouhy/bigdecimal/DoubleTest.java
@@ -8,6 +8,10 @@ public class DoubleTest {
 	public static void main(String args[]){
 		
 		System.out.println(0.01+0.05);
+		System.out.println(1.0-0.42);
+		System.out.println(4.015*100);
+		System.out.println(123.3/100);
+		System.out.println(Math.round(4.015*100)/100.0);
 		
 	}
 }
