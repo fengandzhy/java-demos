14ce7ce7482057faccfb666a480e2a1468dfb6c4
unknown <fengandzhy@gmail.com>
2020-08-10 19:45:25 +0800

    regular expression

--- Java7/Java7-basic/src/main/java/org/zhouhy/regexpressions/RegularExpression1.java
+++ Java7/Java7-basic/src/main/java/org/zhouhy/regexpressions/RegularExpression1.java
@@ -1,5 +1,13 @@
 package org.zhouhy.regexpressions;
 
+
+/**
+ * 正则表达式解读
+ * (ab){1,2}
+ *
+ *
+ *
+ * */
 public class RegularExpression1 {
 
     public static void main(String[] args) {
@@ -20,6 +28,46 @@ public class RegularExpression1 {
 
         System.out.println(str3.matches(reg3));
 
+        String reg4 = "^(?=[a]+\\d).{1,}";
+        String str4 = "a3wwww";
 
+        System.out.println(str4.matches(reg4));
+
+        String reg5 = "(?=[ab]+\\d).{1,2}a";
+        String str5 = "b3a";
+
+        System.out.println(str5.matches(reg5));
+
+
+        String reg6 = "(?=[a]+).{1,2}";
+        String str6 = "aa";
+
+        System.out.println(str6.matches(reg6));
+
+        String reg7 = "industr(?:y|ies)";
+        String str7 = "industry";
+
+        System.out.println(str7.matches(reg7));
+
+        String reg8 = "(?=(ab)).{1,2}";
+        String str8 = "ab";
+
+        System.out.println(str8.matches(reg8));
+
+
+        String reg9 = "(ab).{1,2}";
+        String str9 = "abcd";
+
+        System.out.println(str9.matches(reg9));
+
+        String reg10 = "(?=.*?[A-Z].+).{1,4}";
+        String str10 = "aaBd";
+
+        System.out.println(str10.matches(reg10));
+
+        String reg11 = "(?=A+[A-Z]).{1,12}";
+        String str11 = "ABwwwwwwww";
+
+        System.out.println(str11.matches(reg11));
     }
 }
--- Java7/Java7-basic/src/main/java/org/zhouhy/regexpressions/RegularExpression2.java
+++ Java7/Java7-basic/src/main/java/org/zhouhy/regexpressions/RegularExpression2.java
@@ -7,15 +7,29 @@ public class RegularExpression2 {
 
 
     /**
-     * 
+     *
      *
      * */
     public static void main(String[] args) {
-        Pattern pattern1 = Pattern.compile("a.*?b");
-        Matcher matcher=pattern1.matcher("aacbacb");
+        Pattern pattern1 = Pattern.compile("(a.*?b)");
+        Matcher matcher1=pattern1.matcher("aacbacb");
 
-        while(matcher.find()){
-            System.out.println(matcher.group());
+        while(matcher1.find()){
+            System.out.println(matcher1.group());
+        }
+
+        Pattern pattern2 = Pattern.compile("(?=.*?[A-Z]).{1,8}");
+        Matcher matcher2=pattern2.matcher("edaaBaa");
+
+        while(matcher2.find()){
+            System.out.println(matcher2.group());
+        }
+
+        Pattern pattern3 = Pattern.compile("Aaa(?=[a]+\\d)");
+        Matcher matcher3=pattern3.matcher("Aaaaa3");
+
+        while(matcher3.find()){
+            System.out.println(matcher3.group());
         }
     }
 
