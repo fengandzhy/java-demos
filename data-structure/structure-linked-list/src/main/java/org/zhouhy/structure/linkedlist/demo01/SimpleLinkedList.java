bf939e38a67750681c710a7b07a15a2110c4086a
fengandzhy <fengandzhy@gmail.com>
2021-06-07 12:49:17 +1200

    linked list

--- data-structure/structure-linked-list/src/main/java/org/zhouhy/structure/linkedlist/demo01/SimpleLinkedList.java
+++ data-structure/structure-linked-list/src/main/java/org/zhouhy/structure/linkedlist/demo01/SimpleLinkedList.java
@@ -20,6 +20,47 @@ public class SimpleLinkedList {
             }
         }
     }
+
+    public void addByOrder(HeroNode heroNode){
+        HeroNode temp = this.head;
+        boolean isDuplicate = false;
+        while(true){
+            if(temp.getNext()==null){                
+                break;
+            }
+            if(temp.getNext().getId()>heroNode.getId()){
+                break;
+            }
+            if(temp.getNext().getId() == heroNode.getId()){
+                isDuplicate = true;
+                break;
+            }
+            temp = temp.getNext();
+        }
+        heroNode.setNext(temp.getNext());
+        temp.setNext(heroNode);
+    }
+    
+    public void update(HeroNode node){
+        boolean isExists = false;
+        HeroNode temp = head;
+        while(true){            
+            temp = temp.getNext();
+            if(temp == null){
+                break;
+            }
+            if(temp.getId().equals(node.getId())){
+                isExists = true;
+                break;
+            }
+        }
+        if(isExists){
+            temp.setName(node.getName());
+            temp.setNickname(node.getNickname());
+            return;
+        }
+        System.out.println("没有找到对应节点!");
+    }
     
     public void list(){
         HeroNode temp = this.head;
@@ -27,5 +68,9 @@ public class SimpleLinkedList {
             temp = temp.getNext();
             System.out.println(temp.toString());
         }
+    }
+    
+    public boolean isEmpty(){
+        return head.getNext() == null;
     }
 }
--- data-structure/structure-linked-list/src/test/java/org/zhouhy/structure/linkedlist/demo01/SimpleLinkedListTest.java
+++ data-structure/structure-linked-list/src/test/java/org/zhouhy/structure/linkedlist/demo01/SimpleLinkedListTest.java
@@ -11,6 +11,11 @@ public class SimpleLinkedListTest {
     @Before
     public void setUp(){
         this.linkedList = new SimpleLinkedList();
+        
+    }
+    
+    @Test
+    public void test1(){
         HeroNode node1 = new HeroNode(1,"宋江","及时雨");
         HeroNode node2 = new HeroNode(2,"卢俊义","玉麒麟");
         HeroNode node3 = new HeroNode(3,"吴用","智多星");
@@ -19,10 +24,34 @@ public class SimpleLinkedListTest {
         linkedList.add(node2);
         linkedList.add(node3);
         linkedList.add(node4);
+        linkedList.list();
     }
-    
+
     @Test
-    public void test1(){
+    public void test2(){
+        HeroNode node1 = new HeroNode(1,"宋江","及时雨");
+        HeroNode node2 = new HeroNode(2,"卢俊义","玉麒麟");
+        HeroNode node3 = new HeroNode(3,"吴用","智多星");
+        HeroNode node4 = new HeroNode(4,"公孙胜","入云龙");
+        linkedList.addByOrder(node4);
+        linkedList.addByOrder(node3);
+        linkedList.addByOrder(node2);
+        linkedList.addByOrder(node1);
+        linkedList.list();
+    }
+
+    @Test
+    public void test3(){
+        HeroNode node1 = new HeroNode(1,"宋江","及时雨");
+        HeroNode node2 = new HeroNode(2,"卢俊义","玉麒麟");
+        HeroNode node3 = new HeroNode(3,"吴用","智多星");
+        HeroNode node4 = new HeroNode(4,"公孙胜","入云龙");
+        linkedList.addByOrder(node4);
+        linkedList.addByOrder(node3);
+        linkedList.addByOrder(node2);
+        linkedList.addByOrder(node1);
+        HeroNode node5 = new HeroNode(1,"宋江2","及时雨2");
+        linkedList.update(node5);
         linkedList.list();
     }
 }
