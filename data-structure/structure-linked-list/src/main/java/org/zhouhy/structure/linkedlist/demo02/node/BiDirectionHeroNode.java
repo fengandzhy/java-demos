1b7d7b278bf766381a7c6f889a38a11d9126d0e0
fengandzhy <fengandzhy@gmail.com>
2021-06-09 21:47:37 +1200

    binode

--- data-structure/structure-linked-list/src/main/java/org/zhouhy/structure/linkedlist/demo02/node/BiDirectionHeroNode.java
+++ data-structure/structure-linked-list/src/main/java/org/zhouhy/structure/linkedlist/demo02/node/BiDirectionHeroNode.java
@@ -1,5 +1,17 @@
 package org.zhouhy.structure.linkedlist.demo02.node;
 
 public class BiDirectionHeroNode {
+    private Integer Id;
+    private String name;
+    private String nickname;
+    private BiDirectionHeroNode next;
+    private BiDirectionHeroNode previous;
+    
+    public BiDirectionHeroNode(Integer id, String name, String nickname){
+        this.Id =  id;
+        this.name = name;
+        this.nickname = nickname;
+    }
+    
     
 }
