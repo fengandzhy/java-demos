88ee9940c5ef74da8ad9ab0546506a67461c289f
fengandzhy <fengandzhy@gmail.com>
2021-06-27 04:47:35 +1200

    DoubleLinkedListTest

--- data-structure/structure-linked-list/src/test/java/org/zhouhy/structure/linkedlist/DoubleLikedListTest.java
+++ data-structure/structure-linked-list/src/test/java/org/zhouhy/structure/linkedlist/DoubleLikedListTest.java
@@ -3,6 +3,7 @@ package org.zhouhy.structure.linkedlist;
 import org.junit.Before;
 import org.junit.Test;
 import org.zhouhy.structure.linkedlist.demo03.DoubleLinkedList;
+import org.zhouhy.structure.linkedlist.demo03.node.HeroNode;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,6 +20,21 @@ public class DoubleLikedListTest {
     public void test1(){
         int size = doubleLinkedList.size();
         assertEquals(0, size);
+        HeroNode node = new HeroNode(1,"宋江","及时雨");
+        doubleLinkedList.add(node);
+        size = doubleLinkedList.size();
+        assertEquals(1, size);
+
+        node = new HeroNode(2,"卢俊义","玉麒麟");
+        doubleLinkedList.add(node);
+        size = doubleLinkedList.size();
+        assertEquals(2, size);
+
+        node = new HeroNode(3,"吴用","智多星");
+        doubleLinkedList.add(node);
+        size = doubleLinkedList.size();
+        assertEquals(3, size);
         
+        doubleLinkedList.list();
     }
 }
