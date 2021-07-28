1f9a9f9ecb23b7a16b22a3b384e51e7f22a0614a
fengandzhy <fengandzhy@gmail.com>
2021-07-28 21:32:10 +1200

    optional test

--- java/java-optional/src/test/java/org/zhouhy/java/OptionalTest.java
+++ java/java-optional/src/test/java/org/zhouhy/java/OptionalTest.java
@@ -6,8 +6,7 @@ import org.zhouhy.java.domain.User;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class OptionalTest {
     
@@ -49,5 +48,16 @@ public class OptionalTest {
         User user = new User("john@gmail.com", "1234");
         Optional<User> userOptional = Optional.ofNullable(user);
         userOptional.ifPresent(u->System.out.println(u.getEmail()));
+    }
+
+    @Test
+    public void whenEmptyValue_thenReturnDefault() {
+        User user = null;
+        User user2 = new User("john@gmail.com", "1234");
+        Optional<User> userOptional = Optional.ofNullable(user);
+        assertFalse(userOptional.isPresent());
+        
+        User result = userOptional.orElse(user2);
+        assertEquals(result.getEmail(),user2.getEmail());
     }
 }
