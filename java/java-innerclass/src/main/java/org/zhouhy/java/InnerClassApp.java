f13a0cb4cd7a2a749afbb5b7341b57460dcde9f5
fengandzhy <fengandzhy@gmail.com>
2021-08-08 06:05:31 +1200

    java-innerclass

--- java/java-innerclass/src/main/java/org/zhouhy/java/InnerClassApp.java
+++ java/java-innerclass/src/main/java/org/zhouhy/java/InnerClassApp.java
@@ -7,6 +7,10 @@ package org.zhouhy.java;
  * 
  * 3. 成员内部类中访问外部的同名变量是这样访问的ClassA.this.name
  * 
+ * 4. 局部内部类, 指定义在方法或者代码块里的类, 这种类不能有public和private的修饰, 这种类不能在外部使用
+ * 因为它的定义范围就是定义它的方法或者代码块里面. 例如奔雷的ClassC 里面的ClassD. 外部是没有办法创建
+ * ClassD的, 只能在fun1里面创建
+ * 
  * 
  * */
 public class InnerClassApp {
---
+++ java/java-innerclass/src/main/java/org/zhouhy/java/demo02/ClassCTest.java
@@ -0,0 +1,9 @@
+package org.zhouhy.java.demo02;
+
+public class ClassCTest {
+    public static void main(String[] args) {
+        ClassC c = new ClassC();
+        c.setName("C");
+        c.fun1();
+    }
+}
