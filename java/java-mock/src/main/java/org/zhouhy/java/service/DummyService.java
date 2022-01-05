ac652ffec3cc4ec0e591ec5e622930b2b99dc83d
fengandzhy <fengandzhy@gmail.com>
2022-01-05 21:04:18 +1300

    mockito

--- java/java-mock/src/main/java/org/zhouhy/java/service/DummyService.java
+++ java/java-mock/src/main/java/org/zhouhy/java/service/DummyService.java
@@ -1,11 +1,17 @@
 package org.zhouhy.java.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.zhouhy.java.repository.DummyRepository;
 
 @Service
 public class DummyService {
     
+    @Autowired
+    private DummyRepository repository;
+    
     public String getDummyNameUpperCase(String Id){
+        repository.findById(Id);
         return Id.toUpperCase();
     }
 }
