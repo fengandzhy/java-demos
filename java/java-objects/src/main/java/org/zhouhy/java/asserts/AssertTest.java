32a038fd639643d253c207898f8babd605d5c6e1
fengandzhy <fengandzhy@gmail.com>
2022-01-14 23:10:14 +1300

    asserts

---
+++ java/java-objects/src/main/java/org/zhouhy/java/asserts/AssertTest.java
@@ -0,0 +1,12 @@
+package org.zhouhy.java.asserts;
+
+public class AssertTest {
+    public static void main(String[] args) {
+        checkName(null);
+    }
+    
+    public static void checkName(String name){
+        assert name!=null:"name is empty";
+        System.out.println("We are here!");
+    }
+}
