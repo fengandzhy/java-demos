5d8f3b23ab17f2180d1053b961ac2676f7ff5124
fengandzhy <fengandzhy@gmail.com>
2022-01-15 16:06:02 +1300

    java-reflection

---
+++ java/java-reflection/src/main/java/org/zhouhy/java/domain/Student.java
@@ -0,0 +1,14 @@
+package org.zhouhy.java.domain;
+
+public class Student extends Person{
+    
+    public String stuNo;
+    
+    public Student(String stuNo){
+        this.stuNo = stuNo;
+    }
+    
+    public Student(){
+        
+    }
+}
--- java/java-reflection/src/test/java/org/zhouhy/java/GetFieldsTest.java
+++ java/java-reflection/src/test/java/org/zhouhy/java/GetFieldsTest.java
@@ -2,17 +2,52 @@ package org.zhouhy.java;
 
 import org.junit.Test;
 import org.zhouhy.java.domain.Person;
+import org.zhouhy.java.domain.Student;
 
 import java.lang.reflect.Field;
 
 public class GetFieldsTest {
 
     @Test(expected = Test.None.class /* no exception expected */)
-    public void getFieldsTest(){
+    public void getPersonFieldsTest(){
         Class personClass = Person.class;
         Field[] fields = personClass.getFields();
         for(Field field:fields){
             System.out.println(field);
         }
     }
+    
+    @Test (expected = Test.None.class /* no exception expected */)
+    public void getStudentFieldsTest(){
+        Class<? extends Person> stuClass = Student.class;
+        Field[] fields = stuClass.getFields();
+        for(Field field:fields){
+            System.out.println(field);
+        }
+    }
+
+    @Test (expected = NoSuchFieldException.class)
+    public void getStudentFieldByFieldName() throws NoSuchFieldException {
+        Class<? extends Person> stuClass = Student.class;
+        Field field1 = stuClass.getField("id");
+        System.out.println(field1);
+        Field field2 = stuClass.getField("age");
+        System.out.println(field2);
+        Field field3 = stuClass.getField("stuNo");
+        System.out.println(field3);
+
+        Field field4 = stuClass.getField("rank");
+        System.out.println(field4);
+    }
+
+    @Test (expected = Test.None.class)
+    public void getSetAttributeValueFromField() throws NoSuchFieldException, IllegalAccessException {
+        Class personClass = Person.class;
+        Person person = new Person();
+        Field idField = personClass.getField("id");
+        idField.set(person, 1312120l);
+        System.out.println("id：" + idField.get(person));
+    }
+    
+    
 }
