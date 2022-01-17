276fce6c857faffac49ad82e23071d71d9b20ba9
fengandzhy <fengandzhy@gmail.com>
2022-01-18 10:11:14 +1300

    java factory

---
+++ design-model/design-model-factory/src/main/java/org/zhouhy/desing/model/App.java
@@ -0,0 +1,4 @@
+package org.zhouhy.desing.model;
+
+public class App {
+}
---
+++ design-model/design-model-factory/src/main/java/org/zhouhy/desing/model/demo01/FactoryDemo01Test.java
@@ -0,0 +1,7 @@
+package org.zhouhy.desing.model.demo01;
+
+public class FactoryDemo01Test {
+    public static void main(String[] args) {
+        
+    }
+}
