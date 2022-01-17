f31ca459d7e4c93e1fc9712da2a5bef536edaf60
fengandzhy <fengandzhy@gmail.com>
2022-01-17 21:33:44 +1300

    design model simple factory

--- design-model/design-model-simple-factory/src/main/java/org/zhouhy/desing/model/demo01/Demo01Test.java
+++ design-model/design-model-simple-factory/src/main/java/org/zhouhy/desing/model/demo01/Demo01Test.java
@@ -1,4 +1,11 @@
 package org.zhouhy.desing.model.demo01;
 
+import org.zhouhy.desing.model.demo01.client.PizzaStore;
+
+
 public class Demo01Test {
+    public static void main(String[] args) {
+        PizzaStore store = new PizzaStore();
+        store.orderPizza("meat");
+    }
 }
--- design-model/design-model-simple-factory/src/main/java/org/zhouhy/desing/model/demo01/factory/PizzaSimpleFactory.java
+++ design-model/design-model-simple-factory/src/main/java/org/zhouhy/desing/model/demo01/factory/PizzaSimpleFactory.java
@@ -1,4 +1,21 @@
 package org.zhouhy.desing.model.demo01.factory;
 
+import org.zhouhy.desing.model.demo01.product.Pizza;
+import org.zhouhy.desing.model.demo01.product.impl.MeatPizza;
+import org.zhouhy.desing.model.demo01.product.impl.VegetablePizza;
+
+import java.util.Optional;
+
 public class PizzaSimpleFactory {
+    
+    public static Optional<Pizza> createPizza(String pizzaType){
+        switch (pizzaType){
+            case "meat":
+                return Optional.of(new MeatPizza());
+            case "vegetable":
+                return Optional.of(new VegetablePizza());
+            default: 
+                return Optional.empty();
+        }
+    }
 }
