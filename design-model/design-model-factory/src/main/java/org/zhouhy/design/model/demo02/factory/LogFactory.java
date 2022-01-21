ed6c8fcb8c9d35fcf9f9bc0133401fe36f177bdd
fengandzhy <fengandzhy@gmail.com>
2022-01-21 20:03:02 +1300

    java patterns factory

--- design-model/design-model-factory/src/main/java/org/zhouhy/design/model/demo02/factory/LogFactory.java
+++ design-model/design-model-factory/src/main/java/org/zhouhy/design/model/demo02/factory/LogFactory.java
@@ -2,7 +2,9 @@ package org.zhouhy.design.model.demo02.factory;
 
 import org.zhouhy.design.model.demo02.log.Log;
 
-public interface LogFactory {
-    
+/**
+ * 这是一个抽象工厂，它不负责创建产品，它只是创建产品的共产提供模板
+ * */
+public interface LogFactory {    
     Log createLog();
 }
--- design-model/design-model-factory/src/main/java/org/zhouhy/design/model/demo02/factory/impl/DataBaseLogFactory.java
+++ design-model/design-model-factory/src/main/java/org/zhouhy/design/model/demo02/factory/impl/DataBaseLogFactory.java
@@ -4,10 +4,13 @@ import org.zhouhy.design.model.demo02.factory.LogFactory;
 import org.zhouhy.design.model.demo02.log.Log;
 import org.zhouhy.design.model.demo02.log.impl.DataBaseLog;
 
-public class DataBaseLogFactory implements LogFactory {
-    
+public class DataBaseLogFactory implements LogFactory {    
     /**
-     * 具体的创建DataBaseLog的逻辑
+     * 具体创建DataBaseLog的逻辑,在现实中在创建具体产品的时候可能要实现不同的逻辑, 
+     * 如果我们把具体的创建产品的逻辑暴露给客户端(就是需要使用这些产品的地方)的话, 如果产品创建逻辑发生改变的话,
+     * 那么我们需要在所有用到该产品的地方统一更改逻辑，这样的话，就给代码造成很大匪负担.
+     * 所以我们把创建产品的逻辑封装在这个工厂的代码里, 如果创建逻辑需要改变, 那么我们只需要在工厂里面改.
+     * 
      * */
     @Override
     public Log createLog() {
