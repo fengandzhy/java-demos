08d54ac6d7799fbe66bfbfe783bfca16d06c9fee
fengandzhy <fengandzhy@gmail.com>
2022-02-17 21:52:02 +1300

    char

--- java/java-basic/src/main/java/org/zhouhy/basic/demo01/CharTest.java
+++ java/java-basic/src/main/java/org/zhouhy/basic/demo01/CharTest.java
@@ -8,15 +8,37 @@ public class CharTest {
         System.out.println(toUpperCase('b'));
     }
     
+    /**
+     * 判断是否是字母还是数字
+     * */
     public static boolean isLetterOrDigit(char c){
         return Character.isLetterOrDigit(c);
     }
     
+    /**
+     * 转成小写
+     * */
     public static char toLowerCase(char c){
         return Character.toLowerCase(c);
     }
     
+    /**
+     * 转成大写
+     * */
     public static char toUpperCase(char c){
-        return Character.toUpperCase(c);
+        return Character.toUpperCase(c);        
+    }
+    
+    /**
+     * 可以直接比大小
+     * */
+    public static int compare(char c1, char c2){
+        if( c1 > c2){            
+            return 1;
+        }
+        if(c1 < c2){
+            return -1;
+        }
+        return 0;
     }
 }
