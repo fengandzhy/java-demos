87c150100e811b1892cbcca911dd288824ed7284
fengandzhy <fengandzhy@gmail.com>
2022-05-16 06:12:11 +1200

    java-io

--- java/java-io/src/main/java/org/zhouhy/java/io/FileOutPut.java
+++ java/java-io/src/main/java/org/zhouhy/java/io/FileOutPut.java
@@ -4,6 +4,7 @@ public class FileOutPut {
     public static void main(String[] args) {
         System.out.println(FileOutPut.class.getResource("").getFile());
         System.out.println(Thread.currentThread().getContextClassLoader ().getResource(""));
-        System.out.println(FileOutPut.class.getClassLoader().getResource("").getFile());        
+        System.out.println(FileOutPut.class.getClassLoader().getResource("").getFile());
+        System.out.println(FileOutPut.class.getResource("/txt/a.txt").getFile());        
     }
 }
