a935ea467ad6cb16767310aee967ad261863deae
fengandzhy <fengandzhy@gmail.com>
2022-06-08 18:33:23 +1200

    test

--- design-model/design-model-singleton/src/main/java/org/zhouhy/singleton/md02/Hungary.java
+++ design-model/design-model-singleton/src/main/java/org/zhouhy/singleton/md02/Hungary.java
@@ -2,13 +2,13 @@ package org.zhouhy.singleton.md02;
 
 public class Hungary {
     
-    private static Hungary hungury = new Hungary();
+    private static Hungary hungry = new Hungary();
     
     private Hungary(){
         
     }
     
     public static Hungary getInstance(){
-        return hungury;
+        return hungry;
     }
 }
