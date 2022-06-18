f33033bad12f8a0a418b7e2e880ddf6de6b9a2a7
fengandzhy <fengandzhy@gmail.com>
2022-06-19 10:48:20 +1200

    java io FileReaderTest

--- java/java-io/src/test/java/org/zhouhy/java/io/FileReaderTest.java
+++ java/java-io/src/test/java/org/zhouhy/java/io/FileReaderTest.java
@@ -2,19 +2,25 @@ package org.zhouhy.java.io;
 
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FileReaderTest {
     
+    /**
+     * 1 fileReader.read()一次读取一个字符, 返回的是一个表示该字符的int值, 例如'我’返回的就是25105, 如果返回-1表示读取完毕.
+     * 
+     * */
     @Test
-    public void testFileReader(){
+    public void testFileReader1(){
         String filePath = "d://io//a.txt";
-        FileReader fileReader = null;
-
+        FileReader fileReader;
+        int data;
         try {
             fileReader =  new FileReader(filePath);
+            while((data = fileReader.read())!= -1){
+                System.out.print((char)data);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
