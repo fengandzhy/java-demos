1c8d6053b6c90645765926c5a31ad9319e772fba
fengandzhy <fengandzhy@gmail.com>
2022-06-24 16:28:15 +1200

    java io

---
+++ java/java-io/src/main/java/org/zhouhy/java/io/OutputStreamReader.java
@@ -0,0 +1,19 @@
+package org.zhouhy.java.io;
+
+import java.io.*;
+
+public class OutputStreamReader <T> {
+
+    public T read( String filePath){
+        ObjectInputStream objectInputStream = null;
+        try {
+            objectInputStream = new ObjectInputStream(new FileInputStream(filePath));
+            return (T)objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            IOUtil.close(objectInputStream);
+            return null;
+        }
+    }
+}
--- java/java-io/src/test/java/org/zhouhy/java/io/ObjectOutputStreamTest.java
+++ java/java-io/src/test/java/org/zhouhy/java/io/ObjectOutputStreamTest.java
@@ -11,6 +11,8 @@ import java.util.List;
 
 public class ObjectOutputStreamTest {
     
+    
+    
     /**
      * 1. 当一个对象实现Serializable 的时候, 它就可以被写到ObjectOutputStream 里面, 
      * 如果这个ObjectOutputStream 是由FileOutputStream来构造的话, 那么这个对象就可以写到 file中. 
@@ -19,33 +21,20 @@ public class ObjectOutputStreamTest {
     @Test
     public void testWriteObjectToFile(){
         Employee employee = new Employee("LiLei", 1000);
-        ObjectOutputStream objectOutputStream = null;
-        try {
-            objectOutputStream = 
-                    new ObjectOutputStream(new FileOutputStream("d://io//employee.dat"));
-            objectOutputStream.writeObject(employee);            
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            IOUtil.close(objectOutputStream);
-        }
+        OutputStreamWriter<Employee> outputStreamWriter = new OutputStreamWriter<>();
+        outputStreamWriter.write(employee,"d://io//employee.dat");        
     }
 
     @Test
-    public void testReadObjectFromFile(){        
-        ObjectInputStream objectInputStream = null;
-        try {
-            objectInputStream =
-                    new ObjectInputStream(new FileInputStream("d://io//employee.dat"));
-            Employee employee = (Employee) objectInputStream.readObject();
-            System.out.println(employee.toString());
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            IOUtil.close(objectInputStream);
-        }
+    public void testReadObjectFromFile(){  
+        OutputStreamReader<Employee> reader = new OutputStreamReader<>();
+        Employee employee = reader.read("d://io//employee.dat");
+        System.out.println(employee.toString());        
     }
     
+    /**
+     * 1 如果父类实现了Serializable接口, 则子类也就自动实现这个接口了
+     * */
     @Test
     public void testReadAndWrite(){
         Employee employee = new Employee("LiLei", 1000);
@@ -73,6 +62,13 @@ public class ObjectOutputStreamTest {
         }finally {
             IOUtil.close(objectOutputStream);
         }
-
     }
+    
+    public void testReadAndWriteObjectWithTransient(){
+        
+    }
+    
+    
+    
+    
 }
