8a347bd126e1e57f9960fc94b1cba2c30831f341
fengandzhy <fengandzhy@gmail.com>
2023-07-11 22:17:41 +1200

    java-regular-expression test7

--- java/java-regular-expression/src/test/java/org/frank/java/regular/expression/RegularExpressionTest.java
+++ java/java-regular-expression/src/test/java/org/frank/java/regular/expression/RegularExpressionTest.java
@@ -343,6 +343,13 @@ public class RegularExpressionTest {
         }
     }
 
+    /**
+     * 正向肯定预查 (?=pattern) 表示的是从其当前位置开始，预测后面的字符串必须匹配上pattern
+     * 正向否定预查 (?!pattern) 表示的是从其当前位置开始，预测后面的字符串必须匹配不上pattern
+     * 反向肯定预查 (?<=pattern) 表示的是从其当前位置开始，预测前面的字符串必须匹配上pattern
+     * 反向否定预查 (?<!pattern) 表示的是从其当前位置开始，预测前面的字符串必须匹配不上pattern
+     * 
+     * */
     @Test
     public void test10(){
         String reg1 = ".{2}(?<=95|98|NT|2000)windows";
