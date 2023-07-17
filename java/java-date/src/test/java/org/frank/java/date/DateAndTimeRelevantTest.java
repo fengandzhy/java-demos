68b08bb078dae2cccf68ac6de22b07861c634763
fengandzhy <fengandzhy@gmail.com>
2023-07-17 22:21:33 +1200

    java-date update the name

--- java/java-date/src/test/java/org/frank/java/date/DateAndTimeRelevantTest.java
+++ java/java-date/src/test/java/org/frank/java/date/DateAndTimeRelevantTest.java
@@ -58,7 +58,23 @@ public class DateAndTimeRelevantTest {
         //转换为本地时区：2023年2月11日 下午12:04:03
         System.out.println("当前时间，Locale="+date.toLocaleString());
         //转换为GMT时区：11 Feb 2023 04:04:03 GMT
-        System.out.println("当前时间，GMT="+date.toGMTString());        
-        
+        System.out.println("当前时间，GMT="+date.toGMTString());
+
+        System.out.println("年="+(date.getYear() + 1900)); // 必须加上1900
+
+        System.out.println("月="+(date.getMonth() + 1)); // 0~11，必须加上1
+        System.out.println("日="+date.getDate()); // 1~31，不能加1
+        System.out.println("时="+date.getHours()); // 0~23
+        System.out.println("分="+date.getMinutes()); // 0~59，不能加1
+        System.out.println("秒="+date.getSeconds()); // 0~59，不能加1
+        System.out.println("时间戳="+date.getTime()); // 时间戳，毫秒值
+
+        //构造对象的另一个方法，已过时。year:要减去1900，月份从0开始，0-11；日期是1-31
+        Date d1 =  new Date(1982-1900, 5-1, 1);
+        System.out.println("出生日期="+d1);
+        Date d2 = new Date();
+        System.out.println("出生日期="+d2);
+        long time = d2.getTime() - d1.getTime();
+        System.out.println("已活天数="+time/1000/60/60/24);       
     }
 }
