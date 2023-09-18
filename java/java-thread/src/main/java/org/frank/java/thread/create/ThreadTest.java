84ba1a8bdc4e113390c1c344fecbb4eaa1343ef1
fengandzhy <fengandzhy@gmail.com>
2023-09-19 10:38:11 +1200

    java-collections ComparableTest update

---
+++ java/java-thread/src/main/java/org/frank/java/thread/create/ThreadTest.java
@@ -0,0 +1,9 @@
+package org.frank.java.thread.create;
+
+public class ThreadTest {
+
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+    }
+}
