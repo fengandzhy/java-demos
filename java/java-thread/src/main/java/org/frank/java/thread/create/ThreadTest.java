a3da8d80e262e27520b09692e01e4bdac6a8c57d
fengandzhy <fengandzhy@gmail.com>
2023-09-19 10:42:28 +1200

    java-collections ComparableTest update

--- java/java-thread/src/main/java/org/frank/java/thread/create/ThreadTest.java
+++ java/java-thread/src/main/java/org/frank/java/thread/create/ThreadTest.java
@@ -5,5 +5,10 @@ public class ThreadTest {
     public static void main(String[] args) {
         MyThread thread = new MyThread();
         thread.start();
+        
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread1 = new Thread(myRunnable);
+        thread1.start();
+        
     }
 }
