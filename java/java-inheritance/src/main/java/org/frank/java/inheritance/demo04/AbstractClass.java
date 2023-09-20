191d6d4ef26701573e6f38040e7269929da0347d
fengandzhy <fengandzhy@gmail.com>
2023-09-20 21:49:34 +1200

    java-collections ComparableTest update

---
+++ java/java-inheritance/src/main/java/org/frank/java/inheritance/demo04/AbstractClass.java
@@ -0,0 +1,6 @@
+package org.frank.java.inheritance.demo04;
+
+public abstract class AbstractClass extends ConcreteClass{
+    
+    public abstract void abstractMethod();
+}
---
+++ java/java-inheritance/src/main/java/org/frank/java/inheritance/demo04/ConcreteClass.java
@@ -0,0 +1,8 @@
+package org.frank.java.inheritance.demo04;
+
+public class ConcreteClass {
+
+    public void doSomething() {
+        System.out.println("Doing something in the concrete class.");
+    }
+}
