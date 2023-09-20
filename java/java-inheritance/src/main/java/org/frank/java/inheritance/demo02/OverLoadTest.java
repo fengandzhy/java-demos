745d02bd7536bcbc95a98328edb3816326fc4ab7
fengandzhy <fengandzhy@gmail.com>
2023-09-20 15:21:28 +1200

    java-collections ComparableTest update

---
+++ java/java-inheritance/src/main/java/org/frank/java/inheritance/demo02/OverLoadTest.java
@@ -0,0 +1,17 @@
+package org.frank.java.inheritance.demo02;
+
+public class OverLoadTest {
+    
+    
+    /**
+     * 重载是方法名不同参数列表不同
+     * 
+     * */
+    public void methodA(String a){
+        
+    }
+    
+    public void methodA(String a, Integer b){
+        
+    }
+}
