f9aefea5139920a8ea3a30080533131b98012048
fengandzhy <fengandzhy@gmail.com>
2023-11-20 22:25:08 +1300

    java-collections ComparableTest update

--- java/java-collections/src/test/java/org/frank/java/collections/ListTest.java
+++ java/java-collections/src/test/java/org/frank/java/collections/ListTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class ListTest {
 
     @Test
-    public void RemoveAllTest(){
+    public void removeAllTest(){
         List<Character> characters = new ArrayList<>();
         characters.add('.');
         characters.add('8');
@@ -18,5 +18,27 @@ public class ListTest {
         System.out.println(characters);
         characters.removeAll(characters);
         System.out.println(characters);
+    }
+
+    @Test
+    public void removeDistinctTest1(){
+        List<Integer> list = new ArrayList<Integer>() {{
+            add(1);
+            add(3);
+            add(5);
+            add(2);
+            add(1);
+            add(3);
+            add(7);
+            add(2);
+        }};
+        System.out.println("原集合:" + list);
+        List<Integer> newList = new ArrayList<>();
+        list.forEach(i ->{
+            if(!newList.contains(i)){
+                newList.add(i);
+            }
+        });
+        System.out.println("新集合:" + newList);
     }
 }
