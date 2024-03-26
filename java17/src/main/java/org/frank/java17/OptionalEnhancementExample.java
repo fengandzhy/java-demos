9d6affa1f4d043a9e9e0dc1a8b3e0d7ad7a28758
fengandzhy <fengandzhy@gmail.com>
2024-03-27 05:44:15 +1300

    update the maven plugin.

--- java17/src/main/java/org/frank/java17/OptionalEnhancementExample.java
+++ java17/src/main/java/org/frank/java17/OptionalEnhancementExample.java
@@ -2,6 +2,10 @@ package org.frank.java17;
 
 import java.util.Optional;
 
+/**
+ * ifPresentOrElse 方法是在Java 9中引入到Optional类的。
+ * 这个方法允许你为Optional对象提供两个操作：一个在值存在时执行的操作，另一个在值不存在时执行的操作。
+ * */
 public class OptionalEnhancementExample {
     public static void main(String[] args) {
         Optional<String> optionalPresent = Optional.of("Hello");
