161b6dc61ac28a902bbf8e0890595ed7a01d135e
fengandzhy <fengandzhy@gmail.com>
2024-03-27 05:45:53 +1300

    update the maven plugin.

--- java17/src/main/java/org/frank/java17/OptionalOrExample.java
+++ java17/src/main/java/org/frank/java17/OptionalOrExample.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 /**
  * or方法允许在Optional对象为空时提供一个替代的Optional对象。
+ * or 方法是在Java 9中引入到Optional类中的。这个方法允许在当前Optional对象为空（即不含值）时，提供一个新的Optional对象。
  * */
 public class OptionalOrExample {
     public static void main(String[] args) {
